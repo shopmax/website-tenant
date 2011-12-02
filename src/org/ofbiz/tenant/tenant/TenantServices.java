4c5134e9c861170fc24a67c6b39fe149776095bb
off <off@2b623398-39e6-4a10-b221-8361203ee2be>
2011-12-02 02:37:13 +0000

    create a service to create tenant - [22527] hrs:2.5 ============================================ - initial createTenantDatabase service
    git-svn-id: svn+ssh://antwebsystems.com/tenant/trunk@2 2b623398-39e6-4a10-b221-8361203ee2be

---
+++ src/org/ofbiz/tenant/tenant/TenantServices.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.tenant.tenant;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.Properties;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericDataSourceException;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.datasource.GenericHelperInfo;
+import org.ofbiz.entity.jdbc.ConnectionFactory;
+import org.ofbiz.entity.jdbc.SQLProcessor;
+import org.ofbiz.service.DispatchContext;
+import org.ofbiz.service.ServiceUtil;
+
+/**
+ * Tenant Services
+ * @author chatree
+ *
+ */
+public class TenantServices {
+
+    public final static String module = TenantServices.class.getName();
+    
+    /**
+     * create tenant database
+     * @param ctx
+     * @param context
+     * @return
+     */
+    public static Map<String, Object> createTenantDatabase(DispatchContext ctx, Map<String, Object> context) {
+        Delegator delegator = ctx.getDelegator();
+        String tenantId = (String) context.get("tenantId");
+        String entityGroupName = (String) context.get("entityGroupName");
+        String databaseName = (String) context.get("databaseName");
+        
+        try {
+            GenericValue tenantDataSource = delegator.findOne("TenantDataSource", UtilMisc.toMap("tenantId", tenantId, "entityGroupName", entityGroupName), false);
+            if (UtilValidate.isNotEmpty(tenantDataSource)) {
+                String driverName = tenantDataSource.getString("driverName");
+                String connectionUrl = tenantDataSource.getString("connectionUrl");
+                String userName = tenantDataSource.getString("userName");
+                String password = tenantDataSource.getString("password");
+                Properties props = null;
+                GenericHelperInfo helperInfo = delegator.getGroupHelperInfo(entityGroupName);
+                Connection connection = ConnectionFactory.getConnection(driverName, connectionUrl, props, userName, password);
+                SQLProcessor sqlProcessor = new SQLProcessor(helperInfo, connection);
+                sqlProcessor.executeUpdate("CREATE DATABASE " + databaseName);
+            }
+        } catch (SQLException e) {
+            String errMsg = "Could not create a database for tenant " + tenantId + " with entity group name " + entityGroupName + " : " + e.getMessage();
+            Debug.logError(e, errMsg, module);
+            return ServiceUtil.returnError(errMsg);
+        } catch (GenericDataSourceException e) {
+            String errMsg = "Could not create a database for tenant " + tenantId + " with entity group name " + entityGroupName + " : " + e.getMessage();
+            Debug.logError(e, errMsg, module);
+            return ServiceUtil.returnError(errMsg);
+        } catch (GenericEntityException e) {
+            String errMsg = "Could not create a database for tenant " + tenantId + " with entity group name " + entityGroupName + " : " + e.getMessage();
+            Debug.logError(e, errMsg, module);
+            return ServiceUtil.returnError(errMsg);
+        }
+        return ServiceUtil.returnSuccess();
+    }
+}
