bb9b805b206098137ebdf7486d0db0cb18cbf707
off <off@2b623398-39e6-4a10-b221-8361203ee2be>
2011-12-15 08:11:01 +0000

    create a service to install tenant - [22556] hrs:4 ============================================ create an EECA for TenantDataSource entity to install an individual tenant data source
    git-svn-id: svn+ssh://antwebsystems.com/tenant/trunk@12 2b623398-39e6-4a10-b221-8361203ee2be

--- src/org/ofbiz/tenant/tenant/TenantServices.java
+++ src/org/ofbiz/tenant/tenant/TenantServices.java
@@ -23,20 +23,21 @@ import java.util.List;
 import java.util.Map;
 
 import javolution.util.FastList;
+import javolution.util.FastMap;
 
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.FileUtil;
+import org.ofbiz.base.util.UtilGenerics;
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.entity.Delegator;
 import org.ofbiz.entity.DelegatorFactory;
-import org.ofbiz.entity.GenericDataSourceException;
-import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
 import org.ofbiz.entity.datasource.GenericHelperInfo;
 import org.ofbiz.entity.jdbc.ConnectionFactory;
 import org.ofbiz.entityext.data.EntityDataLoadContainer;
 import org.ofbiz.service.DispatchContext;
+import org.ofbiz.service.LocalDispatcher;
 import org.ofbiz.service.ServiceUtil;
 import org.ofbiz.tenant.jdbc.TenantConnectionFactory;
 import org.ofbiz.tenant.jdbc.TenantJdbcConnectionHandler;
@@ -51,29 +52,26 @@ public class TenantServices {
     public final static String module = TenantServices.class.getName();
     
     /**
-     * install tenant databases
+     * install tenant data source
      * @param ctx
      * @param context
      * @return
      */
-    public static Map<String, Object> installTenantDatabases(DispatchContext ctx, Map<String, Object> context) {
+    public static Map<String, Object> installTenantDataSource(DispatchContext ctx, Map<String, Object> context) {
         Delegator delegator = ctx.getDelegator();
         String tenantId = (String) context.get("tenantId");
         String reader = (String) context.get("reader");
-        
-        List<String> errMsgs = FastList.newInstance();
+        String entityGroupName = (String) context.get("entityGroupName");
+
         try {
             // fist make sure if connection handlers are exist
-            List<GenericValue> tenantDataSources = delegator.findByAnd("TenantDataSource", UtilMisc.toMap("tenantId", tenantId));
-            for (GenericValue tenantDataSource : tenantDataSources) {
-                String entityGroupName = tenantDataSource.getString("entityGroupName");
-                TenantJdbcConnectionHandler connectionHandler = TenantConnectionFactory.getTenantJdbcConnectionHandler(tenantId, entityGroupName, delegator);
-            }
-            
+            TenantJdbcConnectionHandler connectionHandler = TenantConnectionFactory.getTenantJdbcConnectionHandler(tenantId, entityGroupName, delegator);
+        
             String delegatorName = delegator.getDelegatorBaseName() + "#" + tenantId;
-            String[] args = new String[2];
+            String[] args = new String[3];
             args[0] = "-reader=" + reader;
             args[1] = "-delegator=" + delegatorName;
+            args[2] = "-group=" + entityGroupName;
             String configFile = FileUtil.getFile("component://base/config/install-containers.xml").getAbsolutePath();
             
             // load data
@@ -83,21 +81,45 @@ public class TenantServices {
             
             // close connections
             Delegator tenantDelegator = DelegatorFactory.getDelegator(delegatorName);
+            GenericHelperInfo helperInfo = tenantDelegator.getGroupHelperInfo(entityGroupName);
+            Connection connection = ConnectionFactory.getConnection(helperInfo);
+            connection.close();
+        } catch (Exception e) {
+            String errMsg = "Could not install a database for tenant " + tenantId + " with entity group name : " + entityGroupName + " : " + e.getMessage();
+            Debug.logError(e, errMsg, module);
+            return ServiceUtil.returnError(errMsg);
+        }
+        
+        return ServiceUtil.returnSuccess();
+    }
+    
+    /**
+     * install tenant data sources
+     * @param ctx
+     * @param context
+     * @return
+     */
+    public static Map<String, Object> installTenantDataSources(DispatchContext ctx, Map<String, Object> context) {
+        Delegator delegator = ctx.getDelegator();
+        LocalDispatcher dispatcher = ctx.getDispatcher();
+        String tenantId = (String) context.get("tenantId");
+        String reader = (String) context.get("reader");
+        
+        try {
+            List<GenericValue> tenantDataSources = delegator.findByAnd("TenantDataSource", UtilMisc.toMap("tenantId", tenantId));
             for (GenericValue tenantDataSource : tenantDataSources) {
                 String entityGroupName = tenantDataSource.getString("entityGroupName");
-                try {
-                    GenericHelperInfo helperInfo = tenantDelegator.getGroupHelperInfo(entityGroupName);
-                    Connection connection = ConnectionFactory.getConnection(helperInfo);
-                    connection.close();
-                } catch (Exception e) {
-                    String errMsg = "Could not install a database for tenant " + tenantId + " with entity group name : " + entityGroupName + " : " + e.getMessage();
-                    Debug.logError(e, errMsg, module);
-                    errMsgs.add(errMsg);
+                
+                // install tenant data source
+                Map<String, Object> installTenantDataSourceInMap = FastMap.newInstance();
+                installTenantDataSourceInMap.put("tenantId", tenantId);
+                installTenantDataSourceInMap.put("entityGroupName", entityGroupName);
+                installTenantDataSourceInMap.put("reader", reader);
+                Map<String, Object> results = dispatcher.runSync("installTenantDataSource", installTenantDataSourceInMap);
+                if (ServiceUtil.isError(results)) {
+                    List<String> errorMessageList = UtilGenerics.cast(results.get("errorMessageList"));
+                    return ServiceUtil.returnError(errorMessageList);
                 }
-            }
-            
-            if (UtilValidate.isNotEmpty(errMsgs)) {
-                return ServiceUtil.returnError(errMsgs);
             }
         } catch (Exception e) {
             String errMsg = "Could not install databases for tenant " + tenantId + " : " + e.getMessage();
@@ -108,26 +130,52 @@ public class TenantServices {
     }
     
     /**
-     * delete tenant databases
+     * delete tenant data source database
      * @param ctx
      * @param context
      * @return
      */
-    public static Map<String, Object> deleteTenantDatabases(DispatchContext ctx, Map<String, Object> context) {
+    public static Map<String, Object> deleteTenantDataSourceDb(DispatchContext ctx, Map<String, Object> context) {
         Delegator delegator = ctx.getDelegator();
+        String tenantId = (String) context.get("tenantId");
+        String entityGroupName = (String) context.get("entityGroupName");
+        
+        try {
+            TenantJdbcConnectionHandler connectionHandler = TenantConnectionFactory.getTenantJdbcConnectionHandler(tenantId, entityGroupName, delegator);
+            String databaseName = connectionHandler.getDatabaseName();
+            connectionHandler.deleteDatabase(databaseName);
+        } catch (Exception e) {
+            String errMsg = "Could not delete a database for tenant " + tenantId + " with entity group name : " + entityGroupName + " : " + e.getMessage();
+            Debug.logError(e, errMsg, module);
+            return ServiceUtil.returnError(errMsg);
+        }
+        return ServiceUtil.returnSuccess();
+    }
+    
+    /**
+     * delete tenant data source databases
+     * @param ctx
+     * @param context
+     * @return
+     */
+    public static Map<String, Object> deleteTenantDataSourceDbs(DispatchContext ctx, Map<String, Object> context) {
+        Delegator delegator = ctx.getDelegator();
+        LocalDispatcher dispatcher = ctx.getDispatcher();
         String tenantId = (String) context.get("tenantId");
         
         try {
             List<GenericValue> tenantDataSources = delegator.findByAnd("TenantDataSource", UtilMisc.toMap("tenantId", tenantId));
             for (GenericValue tenantDataSource : tenantDataSources) {
                 String entityGroupName = tenantDataSource.getString("entityGroupName");
-                try {
-                    TenantJdbcConnectionHandler connectionHandler = TenantConnectionFactory.getTenantJdbcConnectionHandler(tenantId, entityGroupName, delegator);
-                    String databaseName = connectionHandler.getDatabaseName();
-                    connectionHandler.deleteDatabase(databaseName);
-                } catch (Exception e) {
-                    String errMsg = "Could not delete a database for tenant " + tenantId + " with entity group name : " + entityGroupName + " : " + e.getMessage();
-                    Debug.logError(e, errMsg, module);
+                
+                // delete tenant data source
+                Map<String, Object> installTenantDataSourceInMap = FastMap.newInstance();
+                installTenantDataSourceInMap.put("tenantId", tenantId);
+                installTenantDataSourceInMap.put("entityGroupName", entityGroupName);
+                Map<String, Object> results = dispatcher.runSync("deleteTenantDataSource", installTenantDataSourceInMap);
+                if (ServiceUtil.isError(results)) {
+                    List<String> errorMessageList = UtilGenerics.cast(results.get("errorMessageList"));
+                    return ServiceUtil.returnError(errorMessageList);
                 }
             }
             
@@ -135,11 +183,7 @@ public class TenantServices {
             if (UtilValidate.isNotEmpty(errMsgs)) {
                 return ServiceUtil.returnError(errMsgs);
             }
-        } catch (GenericDataSourceException e) {
-            String errMsg = "Could not delete databases for tenant " + tenantId + " : " + e.getMessage();
-            Debug.logError(e, errMsg, module);
-            return ServiceUtil.returnError(errMsg);
-        } catch (GenericEntityException e) {
+        } catch (Exception e) {
             String errMsg = "Could not delete databases for tenant " + tenantId + " : " + e.getMessage();
             Debug.logError(e, errMsg, module);
             return ServiceUtil.returnError(errMsg);
