4e73fec9514faedbeeb946030d00497ec9355a85
off <off@2b623398-39e6-4a10-b221-8361203ee2be>
2012-02-17 03:47:45 +0000

    make component options in the screen and event - [23619] hrs:2 ============================================================== - add ComponentWorker class with these methods:   * getFrameworkComponentNames   * getApplicationsComponentNames   * getSpecialPurposeComponentNames   * getEntityDataUrlStringByComponentList - use the ComponentWorker class in the TenantEvents.xml file
    git-svn-id: svn+ssh://antwebsystems.com/tenant/trunk@95 2b623398-39e6-4a10-b221-8361203ee2be

---
+++ src/org/ofbiz/tenant/component/ComponentWorker.java
@@ -0,0 +1,131 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.tenant.component;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+
+import org.ofbiz.base.util.StringUtil;
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.base.util.UtilXml;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.datasource.GenericHelperInfo;
+import org.ofbiz.entity.util.EntityDataLoader;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javolution.util.FastList;
+
+/**
+ * Component Worker
+ * @author chatree
+ *
+ */
+public class ComponentWorker {
+
+    public final static String module = ComponentWorker.class.getName();
+    
+    /**
+     * get framework component names
+     * @return
+     * @throws Exception
+     */
+    public static List<String> getFrameworkComponentNames() throws Exception {
+        String ofbizHome = System.getProperty("ofbiz.home");
+        File componentLoadFile = new File(ofbizHome, "framework" + File.separatorChar + "component-load.xml");
+        Document document = UtilXml.readXmlDocument(componentLoadFile.toURI().toURL());
+        List<? extends Element> elements = UtilXml.childElementList(document.getDocumentElement());
+        List<String> componentNames = FastList.newInstance();
+        for (Element element : elements) {
+            String componentName = UtilXml.elementAttribute(element, "component-location", null);
+            componentNames.add(componentName);
+        }
+        return componentNames;
+    }
+    
+    /**
+     * get applications component names
+     * @return
+     * @throws Exception
+     */
+    public static List<String> getApplicationsComponentNames() throws Exception {
+        String ofbizHome = System.getProperty("ofbiz.home");
+        File componentLoadFile = new File(ofbizHome, "applications" + File.separatorChar + "component-load.xml");
+        Document document = UtilXml.readXmlDocument(componentLoadFile.toURI().toURL());
+        List<? extends Element> elements = UtilXml.childElementList(document.getDocumentElement());
+        List<String> componentNames = FastList.newInstance();
+        for (Element element : elements) {
+            String componentName = UtilXml.elementAttribute(element, "component-location", null);
+            componentNames.add(componentName);
+        }
+        return componentNames;
+    }
+    
+    /**
+     * get specialpurpose component names
+     * @return
+     * @throws Exception
+     */
+    public static List<String> getSpecialPurposeComponentNames() throws Exception {
+        String ofbizHome = System.getProperty("ofbiz.home");
+        File componentLoadFile = new File(ofbizHome, "specialpurpose" + File.separatorChar + "component-load.xml");
+        Document document = UtilXml.readXmlDocument(componentLoadFile.toURI().toURL());
+        List<? extends Element> elements = UtilXml.childElementList(document.getDocumentElement());
+        List<String> componentNames = FastList.newInstance();
+        for (Element element : elements) {
+            String componentName = UtilXml.elementAttribute(element, "component-location", null);
+            componentNames.add(componentName);
+        }
+        return componentNames;
+    }
+    
+    /**
+     * get entity data URL string by component list
+     * @param entityGroupName
+     * @param components
+     * @param readers
+     * @param delegator
+     * @return
+     */
+    public static String getEntityDataUrlStringByComponentList(String entityGroupName, List<String> components, String readers, Delegator delegator) {
+        GenericHelperInfo helperInfo = delegator.getGroupHelperInfo(entityGroupName);
+        String helperName = helperInfo.getHelperBaseName();
+        List<String> readerNames = StringUtil.split(readers, ",");
+        
+        // get URL list from the component list
+        List<URL> urls = null;
+        if (UtilValidate.isNotEmpty(readerNames)) {
+            urls = EntityDataLoader.getUrlByComponentList(helperName, components, readerNames);
+        } else {
+            urls = EntityDataLoader.getUrlByComponentList(helperName, components);
+        }
+        
+        // build a String from the list of URL
+        StringBuilder filesBuilder = new StringBuilder();
+        for (URL url : urls) {
+            filesBuilder.append(url.toString() + ",");
+        }
+        
+        //get rid of the last comma
+        String files = filesBuilder.toString().substring(0, filesBuilder.toString().length() - 1);
+        
+        return files;
+    }
+}
